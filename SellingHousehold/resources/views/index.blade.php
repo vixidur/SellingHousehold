c0a117507e242323ad47bc431e62c9200b035925
vixidurr2402 <chien27122402@gmail.com>
2024-10-16 00:21:51 +0700

    update header, footer

---
+++ SellingHousehold/resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UNETI HOUSEHOLD | BÁN ĐỒ GIA DỤNG UNETI</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('images/logo-home.png') }}" type="image/x-icon" />
+</head>
+
+<body>
+    @include('layouts.header')
+    <div class="content">
+        <center>
+            <h1>Welcome to the Homepage</h1>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+            <p>Nội dung chính ở đây nè</p>
+
+        </center>
+    </div>
+    @include('layouts.footer')
+</body>
+
+</html>

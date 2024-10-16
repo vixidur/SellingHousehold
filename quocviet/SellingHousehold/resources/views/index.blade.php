9c2e183f5d232a36001569fd751a9c899d2dde26
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-16 23:28:50 +0700

    update main content

---
+++ quocviet/SellingHousehold/resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UNETI HOUSEHOLD | BÁN ĐỒ GIA DỤNG UNETI</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('images/logo-home.png') }}" type="image/x-icon" />
+</head>
+
+<body>
+    @if (Route::currentRouteName() !== 'login')
+        @include('layouts.header')
+    @endif
+    @include('maincontent.main')
+    @if (Route::currentRouteName() !== 'login')
+        @include('layouts.footer')
+    @endif
+</body>
+
+</html>

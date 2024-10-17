ba7e82ad75325e4915909976ef6210aec2f204d8
THVHTNH <vulongh85@gmail.com>
2024-10-17 22:31:16 +0700

    update fodel cart

--- SellingHousehold/routes/web.php
+++ SellingHousehold/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CartController;
 
 // Đường dẫn cho các route
 Route::get('/', function () {
@@ -10,4 +12,8 @@ Route::get('/', function () {
 });
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::get('register', [RegisterController::class, 'showRegisterForm'])->name('register');
+
+Route::get('cart', [CartController::class, 'cartForm'])->name('cart');
+
 Route::post('register', [RegisterController::class, 'register']);
+

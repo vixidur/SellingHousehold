806f66ec9b9a24492a3a06b91da75f67c73fc993
vixidurr2402 <chien27122402@gmail.com>
2024-10-17 17:05:26 +0700

    Lưu các thay đổi trong main.js

--- SellingHousehold/public/js/main.js
+++ SellingHousehold/public/js/main.js
@@ -1,14 +1,12 @@
 const listImage = document.querySelector('.list-image');
 const imgs = document.querySelectorAll('.swiper-slide');
 let currentIndex = 0;
-
 const displayTime = 3000;
 const transitionTime = 1000;
 
 function showNextSlide() {
     const width = window.innerWidth;
 
-    // Thêm các lớp CSS để tạo hiệu ứng "swiper-slide-prev" và "swiper-slide-next"
     imgs.forEach((img, index) => {
         img.classList.remove('swiper-slide-prev', 'swiper-slide-next', 'swiper-slide-active');
 
@@ -21,14 +19,11 @@ function showNextSlide() {
         }
     });
 
-    // Di chuyển đến slide hiện tại
     listImage.style.transition = `transform ${transitionTime}ms ease-in-out`;
     listImage.style.transform = `translateX(${-currentIndex * width}px)`;
 
-    // Cập nhật chỉ số slide
     currentIndex = (currentIndex + 1) % imgs.length;
 
-    // Sau khi đạt đến cuối, quay lại ảnh đầu tiên
     if (currentIndex === imgs.length - 1) {
         setTimeout(() => {
             listImage.style.transition = 'none';
@@ -37,16 +32,13 @@ function showNextSlide() {
         }, transitionTime);
     }
 
-    // Lặp lại sau thời gian hiển thị
     setTimeout(showNextSlide, displayTime);
 }
 
-// Khởi động slideshow
 setTimeout(showNextSlide, displayTime);
 
-// Điều chỉnh lại khi kích thước màn hình thay đổi
 window.addEventListener('resize', () => {
     const width = window.innerWidth;
-    listImage.style.transition = 'none'; // Loại bỏ hiệu ứng chuyển đổi khi thay đổi kích thước
+    listImage.style.transition = 'none';
     listImage.style.transform = `translateX(${-currentIndex * width}px)`;
 });
